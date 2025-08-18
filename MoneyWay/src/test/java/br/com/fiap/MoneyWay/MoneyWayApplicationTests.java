3a9d8773d270c413eb499634787e99e8ba032306
joaoGFG <joaogabrielfg10@gmail.com>
2025-08-18 10:53:51 -0300

    chore: initial commit

---
+++ MoneyWay/src/test/java/br/com/fiap/MoneyWay/MoneyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.MoneyWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
