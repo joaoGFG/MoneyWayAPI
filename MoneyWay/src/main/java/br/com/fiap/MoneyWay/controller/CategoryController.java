423b53eb285535cbb3fe6ab77391bc0e3a51e29e
joaoGFG <joaogabrielfg10@gmail.com>
2025-08-21 09:42:28 -0300

    feat: Status code and logs

--- MoneyWay/src/main/java/br/com/fiap/MoneyWay/controller/CategoryController.java
+++ MoneyWay/src/main/java/br/com/fiap/MoneyWay/controller/CategoryController.java
@@ -2,24 +2,39 @@ package br.com.fiap.MoneyWay.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.MoneyWay.model.Category;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
 @RestController
+@Slf4j
 public class CategoryController {
+
+    private List<Category> repository = new ArrayList<>();
 
     @GetMapping("/categories")
     public List<Category> index(){
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Leisure", "movie"));
-        categories.add(new Category(2L, "Transport", "bus"));
-
-        return categories;
+        return repository;
     }
-    
+
+
+    @PostMapping("/categories")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Category create(@RequestBody Category category){
+        category.setId(Math.abs(new Random().nextLong()));
+        repository.add(category);
+        return category;
+    }
+
 }
--- MoneyWay/src/main/java/br/com/fiap/MoneyWay/model/Category.java
+++ MoneyWay/src/main/java/br/com/fiap/MoneyWay/model/Category.java
@@ -1,26 +1,13 @@
 package br.com.fiap.MoneyWay.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Category {
     private Long id;
     private String name;
     private String icon;
-
-    public Category(Long id, String name, String icon) {
-        this.id = id;
-        this.name = name;
-        this.icon = icon;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
 
 }
